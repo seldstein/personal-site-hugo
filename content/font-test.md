edf2e8a6895c042c684933c247ae08738bfac569
Sam Feldstein <samuelfeldstein@proton.me>
2024-01-07 22:59:10 -0600

    Add additional font styles

---
+++ content/font-test.md
@@ -0,0 +1,16 @@
+---
+title: Font Test
+date: 2024-01-07T22:36:05-06:00
+description:
+tags:
+  - 
+draft: true
+---
+
+Normal
+
+**Bold.**
+
+*Italic.*
+
+***Bold Italic.***
--- themes/custom/offline.js
+++ themes/custom/offline.js
@@ -1,30 +1,30 @@
-// On install, cache some stuff
-addEventListener('install', function (event) {
-	event.waitUntil(caches.open('core').then(function (cache) {
-		cache.add(new Request('offline/index.html'));
-		return;
-	}));
-});
+// // On install, cache some stuff
+// addEventListener('install', function (event) {
+// 	event.waitUntil(caches.open('core').then(function (cache) {
+// 		cache.add(new Request('offline/index.html'));
+// 		return;
+// 	}));
+// });
 
-// listen for requests
-addEventListener('fetch', function (event) {
+// // listen for requests
+// addEventListener('fetch', function (event) {
 
-	// Get the request
-	var request = event.request;
+// 	// Get the request
+// 	var request = event.request;
 
-	// Bug fix
-	// https://stackoverflow.com/a/49719964
-	if (event.request.cache === 'only-if-cached' && event.request.mode !== 'same-origin') return;
+// 	// Bug fix
+// 	// https://stackoverflow.com/a/49719964
+// 	if (event.request.cache === 'only-if-cached' && event.request.mode !== 'same-origin') return;
 
-	// HTML files
-	// Network-first
-	if (request.headers.get('Accept').includes('text/html')) {
-		event.respondWith(
-			fetch(request).then(function (response) {
-				return response;
-			}).catch(function (error) {
-				return caches.match('offline/index.html');
-			})
-		);
-	}
-});
+// 	// HTML files
+// 	// Network-first
+// 	if (request.headers.get('Accept').includes('text/html')) {
+// 		event.respondWith(
+// 			fetch(request).then(function (response) {
+// 				return response;
+// 			}).catch(function (error) {
+// 				return caches.match('offline/index.html');
+// 			})
+// 		);
+// 	}
+// });
