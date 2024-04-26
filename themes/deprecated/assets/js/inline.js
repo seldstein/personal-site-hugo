2b68c581cef5a62491f744eea17aebc6c062e9d1
Sam Feldstein <106436328+seldstein@users.noreply.github.com>
2024-04-26 15:18:23 -0500

    Fix menu partial
    Was rendering nested lists.

--- themes/deprecated/assets/js/inline.js
+++
@@ -1,9 +0,0 @@
-// || Service worker
-
-// https://web.dev/learn/pwa/service-workers
-// https://github.com/mdn/serviceworker-cookbook/blob/master/offline-fallback/index.js
-
-// If the browser supports service workers, register the service worker
-if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register("/service-worker.js");
- } 
